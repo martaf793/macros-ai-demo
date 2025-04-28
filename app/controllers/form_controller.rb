87745aa7b7e5bfda5f9920c7222fe14bd08b6f07
martaf793 <martaf@uchicago.edu>
2025-04-28 19:54:55 +0000

    assistant

--- app/controllers/form_controller.rb
+++ app/controllers/form_controller.rb
@@ -7,6 +7,18 @@ class FormController < ApplicationController
     @the_image=params.fetch("image_param", "not there")
     # @the_image=params.fetch["image_param"] #esto te devuelve nil
     @description=params.fetch("description_param")
+
+    c=OpenAI::Chat.new 
+      c.system("your are an expert nutritionist. The user will give you an image and/or description of a meal")
+      @description
+      
+      c.user(@description)
+      c.schema='{
+      "name"=" nutrition_facts"
+      }'
+      @structured_output=c.assistant!
+      c.assistant!
     render({ :template => "form_templates/p_results" })
+
   end
 end
