fe84443b4f388d03705415046ec2fcd24342b15b
martaf793 <martaf@uchicago.edu>
2025-04-28 19:16:04 +0000

    image

--- app/controllers/form_controller.rb
+++ app/controllers/form_controller.rb
@@ -1,6 +1,13 @@
 class FormController < ApplicationController
   def blank_form
 
-  render({ :template => "form_templates/form_trial" })
+    render({ :template => "form_templates/form_trial" })
+  end
+  def proc_inputs
+    @the_image=params.fetch("image_param")
+    @description=params.fetch("description_param")
+    render({ :template => "form_templates/p_results" })
+
+    
   end
 end
